ab08b5f3296f7dc993ac14ee89cd3aff02ae4f94
turtle0910 <brianna.guest@mymail.champlain.edu>
2019-02-24 22:06:11 -0500

    updated secure-shh.sh file

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -3,12 +3,18 @@
 #!/bin/bash
 
 #Creates a new ssh user called $l
+read l
 
+sudo useradd $l
+sudo usermod -aG wheel $l
+sudo mkdir /home/$l/.ssh
 
-#Adds a public key to that users authorized keys file
+#Adds a public key to that users authorized key file
+sudo cp linux/public-keys/SYS265.pub /home/$l/.ssh/authorized_keys
 
-
-#Removes roots ability to ssh in
-
+#Removes roots ablility to ssh in
+sudo chmod 700 /home/$l/.ssh
+sudo chmod 644 /home/$l/.ssh/authorized_keys
+sudo chown -R $l:$l /home/$l/.ssh
 
 echo "testing 123"
